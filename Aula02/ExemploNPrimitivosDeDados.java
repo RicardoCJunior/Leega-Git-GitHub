ea90f774120739e85470c05b41a0f09a3eb3cef0
Ricardo Junior <seuemail@gmail.com>
2026-02-12 22:02:10 -0300

    Aula 02 Variaveis

---
+++ Aula02/ExemploNPrimitivosDeDados.java
@@ -0,0 +1,26 @@
+package Aula02;
+
+public class ExemploNPrimitivosDeDados {
+    public static void main(String[] args){
+
+    char caracter ='A';
+
+    String nome = "Ricardo";
+
+    nome = "Ricardo Cavalcante";
+
+    enum DiaDaSemana { //coleção de palavras que se repete
+        DOMINGO, SEGUNDA, TERÇA, QUARTA, QUINTA, SEXTA, SÁBADO
+    }
+    DiaDaSemana dia = DiaDaSemana.SEGUNDA;
+
+    int[] numeros = {1, 2, 3, 4, 5}; //começa sempre em 0
+
+    System.out.println("Valor do caracter: " + caracter);
+    System.out.println("Valor do nome: " + nome);
+    System.out.println("Valor do dia da semana: " + dia);
+    System.out.println("Valor do array numeros: " + numeros[0]); 
+
+    }
+    
+}
---
+++ Aula02/ExemplosPrimitivosDeDados.java
@@ -0,0 +1,29 @@
+package Aula02;
+public class ExemplosPrimitivosDeDados {
+    public static void main(String[] args){
+        byte valorByte = 127;
+        short valorShort = 32767;
+        int valorInt = 2147483647;
+        long valorLong = 9223372036854775807L;
+
+        float valorFloat = 3.14f;
+        double valorDouble = 3.141592653589793;
+
+        char valorChar = 'A';
+        boolean valorBoolean = true;
+
+        final float PI = 3.14f; // Constante não vai ser alterado
+        // vPi = 3.1415f; // Erro, não pode alterar o valor de uma constante
+
+        System.out.println("Valor byte: " + valorByte);
+        System.out.println("Valor short: " + valorShort);
+        System.out.println("Valor int: " + valorInt);
+        System.out.println("Valor long: " + valorLong);
+        System.out.println("Valor float: " + valorFloat);
+        System.out.println("Valor double: " + valorDouble);
+        System.out.println("Valor char: " + valorChar);
+        System.out.println("Valor boolean: " + valorBoolean);
+        System.out.println("Valor de PI: " + PI);
+        
+    }
+}
